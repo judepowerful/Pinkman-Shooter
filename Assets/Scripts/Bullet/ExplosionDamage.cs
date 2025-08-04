1d460732f9cf3388678baa99e9864e1750a0ebd5
judepowerful <71837161+judepowerful@users.noreply.github.com>
2025-08-05 00:24:24 +0800

    RPG finished

--- Assets/Scripts/Bullet/ExplosionDamage.cs
+++ Assets/Scripts/Bullet/ExplosionDamage.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class ExplosionDamage : MonoBehaviour
 {
-    public float damageRadius = 2f;
-    public int damageAmount = 1;
-    public float explosionForce = 10f;
+    [Header("Explosion Settings / 爆炸设置")]
+    [SerializeField] private float damageRadius = 2f;
+    [SerializeField] private int damageAmount = 1;
+    [SerializeField] private float explosionForce = 10f;
+    [SerializeField] private GameObject smokeEffectPrefab;
     public BulletTeam team;
 
     private void Start()
     {
+        GameObject smokeEffect = Instantiate(smokeEffectPrefab, transform.position, Quaternion.identity);
+        Destroy(smokeEffect, 10f);
+        
         CameraShake.Instance.Shake(0.5f, 0.3f); // 震动效果
         // 获取范围内所有碰撞体
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, damageRadius);
@@ -16,16 +21,18 @@ public class ExplosionDamage : MonoBehaviour
         foreach (var hit in hits)
         {
             Vector2 dir = (hit.transform.position - transform.position).normalized;
+            // 向上偏移 1 倍单位向量
+            dir += new Vector2(dir.x, 1f);
+            dir.Normalize();
 
             if (team == BulletTeam.Player && hit.CompareTag("Enemy"))
             {
                 // 伤害
-                hit.GetComponent<Enemy>()?.TakeDamage(damageAmount);
+                hit.GetComponent<Enemy>().TakeDamage(damageAmount);
 
                 // 推力
                 Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
-                if (rb != null)
-                    rb.AddForce(dir * explosionForce, ForceMode2D.Impulse);
+                rb.AddForce(dir * explosionForce, ForceMode2D.Impulse);
             }
             else if (team == BulletTeam.Enemy && hit.CompareTag("Player"))
             {
@@ -36,9 +43,6 @@ public class ExplosionDamage : MonoBehaviour
                     rb.AddForce(dir * explosionForce, ForceMode2D.Impulse);
             }
         }
-
-        // 自动销毁特效体
-        Destroy(gameObject, 0.5f);
     }
 
     private void OnDrawGizmosSelected()
